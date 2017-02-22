4ce15d6efd893b4a035e20527d33af06596c7d5a
Frank Stollmeier <franks@nld.ds.mpg.de>
2017-02-22 14:12:06 +0100

    Initial commit

---
+++ article/stollmeier-2017.md
@@ -0,0 +1,93 @@
+---
+Title: "A simple rule for the evolution of cooperation on graphs and social networks"
+Author:
+  - name: Frank Stollmeier
+    affiliation: 1, 2
+Address:
+  - code:    1
+    address: Network Dynamics, Max Planck Institute for Dynamics and Self-Organization (MPIDS), Am Faßberg 17, 37077 Göttingen, Germany
+  - code:    2
+    address: Institute for Nonlinear Dynamics, Faculty of Physics, University of Göttingen, Am Fassberg 17, 37077 Göttingen, Germany
+Contact:
+  - franks@nld.ds.mpg.de
+Editor:
+  - Name Surname
+Reviewer:
+  - Name Surname
+  - Name Surname
+Publication:
+  received:  xxx,  x, xxxx
+  accepted:  xxx, x, xxxx
+  published: xxx, x, xxxx
+  volume:    "**x**"
+  issue:     "**x**"
+  date:      xxx xxx
+Repository:
+  article:   "http://github.com/rescience/rescience-submission/article"
+  code:      "http://github.com/rescience/rescience-submission/code"
+  data:      
+  notebook:  
+Reproduction:
+  - "A simple rule for the evolution of cooperation on graphs and social networks. H. Ohtsuki, C. Hauert, E. Lieberman and M.A. Nowak. Nature ***441*** (2006)."
+Bibliography:
+  bibliography.bib
+
+---
+
+# Introduction
+
+A central question in evolutionary game theory is how cooperation can evolve in a Prisoner's Dilemma.
+In an unstructured population the defectors have always a higher payoff than the cooperators, hence natural selection leads to extinction of the cooperators. One of the mechanisms that promote cooperation is network reciprocity. If the individuals do not interact with all other individuals but only with a subset of the other individuals, e.g. the adjacent individuals in a social network, then cooperators can form groups with other cooperators, which increases the benefit from other cooperators and reduces the exploitation from defectors. As a consequence, the fixation probability of cooperation increases. Cooperation is said to be favored by natural selection if this probability exceeds $1/N$, the fixation probability of a neutral mutation in a population of size $N$. 
+
+Ohtsuki et al. [@Ohtsuki2006] showed that a simple rule indicates whether cooperation is favored. The fixation probability of cooperators exceeds $1/N$ if the condition $b/c>k$ is satisfied. The parameter $c$ is the cost an individual pays to cooperate with others, $b$ is the benefit the other individual receives from a cooperator, and $k$ is the average degree of the network. They compare this rule with fixation probabilities measured in numerical simulations. The rule is most accurate for regular networks with large populations. It is less accurate, but still valid, for networks in which the node degree varies, e.g. random or scale-free networks, and for networks with small population sizes.
+Until today, the question of the critical benefit-to-cost ratio in networks attracted much interest. Further proofs of this rule, more precise rules, exact calculations and generalizations were published [@Taylor2007; @Konno2011; @Chen2013; @Allen2014; @Allen2016].
+
+This paper presents an implementation to reproduce the numerical results of the original paper based solely on the information given in the original paper.
+The computationally intensive algorithms are written in Cython. A Jupyter Notebook contains the Python scripts to produce all figures. Since accurate measurements of the fixation probability require a large number of simulations for each set of parameters, these scripts are prepared to run simulations in parallel using the module ipyparallel.
+
+
+# Methods
+
+## Overview
+
+In order to measure the fixation probability for a certain graph type and specific parameters, we generate 1000 graphs and run 1000 simulations on each graph. 
+Each simulation starts with the initialization of the strategies. All individuals are set to 'defect', except for one randomly chosen individual that is set to 'cooperate'. After the initialization the death-birth updating or imitation updating is repeated until all individuals have the same strategy. The fixation probability is the ratio of the number of simulations in which all individuals cooperate at the end and the total number of the simulations. Cooperation is expected to be favored if $b/c>k$ for the death-birth updating and if $b/c>k+2$ for imitation updating.
+We use the same graph types as described in the supplementary of the original paper: circle graphs, lattice graphs, random regular graphs, random graphs and scale-free graphs.
+
+## Death-birth update
+
+A death-birth update consists of two steps. First, a random individual is chosen to die. Second, the adjacent individuals compete for filling the vacant site with a copy of themselves. If the adjacent cooperators have the total fitness $F_C$ and the adjacent defectors have the total fitness $F_D$, the probability that the new individual is a cooperator is $\frac{F_C}{F_C+F_D}$. The fitness of an individual is $f = 1-w+wP_X$. The parameter $w$ is the selection strength and throughout this study set to $0.01$ ("weak selection"). The payoff $P_X$ depends on the strategy $X\in \{C,D\}$ of the individual, the number of adjacent cooperators $i$ and the number of adjacent defectors $j$. A cooperator gets $P_C=bi-c(i+j)$ and a defector $P_D=bi$, where $b$ is the benefit from a cooperator and $c$ the cost of cooperation. Note that the individual that is chosen to die is included when evaluating the fitness of the adjacent individuals. In other words, the fitness is evaluated before the death step.
+
+## Imitation updating
+
+First, a random individual is chosen to update its strategy. Including to the total fitnesses $F_C$ and $F_D$ of the adjacent cooperators and defectors the imitation update also depends on the own fitness $f_0$ of the selected individual. With the probability $\frac{F_C}{F_C+F_D+f_0}$ it will imitate the cooperation strategy and with the probability $\frac{F_D}{F_C+F_D+f_0}$ it will imitate the defection strategy.  
+
+
+
+# Results
+
+In the end we are interested in how the fixation probability depends on the benefit-to-cost ratio $b/c$, but for the simulations we need to choose absolute values for the costs $c$ and the benefit $b$. Since neither of those values is given in the original paper, we first study how the choice of $c$ affects the results.
+As an example, we measure the fixation probability curves for the cycle graph with $N=500$ nodes and degree $k=10$ for different choices of $c$. 
+The results are shown in figure @fig:c_estimation, together with data taken from figure 2a of the original paper for comparison. 
+
+Most importantly, the position of the intersection of the fixation probability curves with the fixation probability of a neutral mutation is almost unaffected by the choice of the absolute value $c$. Assuming that this is also true for other graph types, this means that we can choose (within a certain range) arbitrary absolute values. The curve with $c=0.125$ matches the data of the same graph type from Ohtsuki et al. We will use this value for all further simulations. 
+
+![Fixation probability curves for a cycle graph with $N=500$ nodes and degree $k=10$. The colored dots are measured with different absolute cost values, the black dots are taken from figure 2a of the original paper.](./figures/cost_value_estimation.pdf){#fig:c_estimation}
+
+Figure @fig:fp_db shows the fixation probability curves for the death-birth process. As expected from the theory, all curves cross the line of a neutral mutation at benefit-to-cost ratios greater than $k$. With larger networks the intersection in much closer to $b/c=k$. 
+This effect is much weaker with scale-free networks than with the other network types. These observations are consistent with the original paper. The only visible difference is that the curves in original paper are much steeper.
+
+Figure @fig:fp_im shows the fixation probability curves for imitation updating with small network sizes only, as in the original paper. All curves cross the line of a neutral mutation at benefit-to-cost ratios greater than $k+2$. Again, the difference to the original paper is that the curves in the original paper are much steeper. 
+
+![Fixation probability curves using death-birth updating with the same graph types and parameters as in Figure 2 of the original paper. The only parameter that is different to the original paper is the absolute cost value $c$, which is $0.125$ here and unknown in the original paper. The horizontal black dashed lines indicate the fixation probability of a neutral mutation $1/N$, the vertical colored lines are at $b/c = k$.](./figures/fixation_probabilities.pdf){#fig:fp_db}
+
+![Fixation probability curves using imitation updating with the same graph types and parameters as in Figure 4 of the supplementary of the original paper. The only parameter that is different to the original paper is the absolute cost value $c$, which is $0.125$ here and unknown in the original paper. The horizontal black dashed lines indicate the fixation probability of a neutral mutation $1/N$, the vertical colored lines are at $b/c = k+2$.](./figures/fixation_probabilities_im.pdf){#fig:fp_im}
+
+# Conclusion
+
+The steeper curves in the original paper are likely a result of larger absolute cost values. The test with different values showed that the absolute cost value changes the slope of the curve, but has almost no effect on the position of the intersection of the fixation probability curve with the fixation probability of a neutral mutation. For all considered graph types, network sizes and degrees the positions of the intersection are consistent with the numerical results presented in the original paper and are consistent with their theoretical results, $b/c>k$ for death-birth updating and $b/c>k+2$ for imitation updating.
+In summary, the results of this implementation confirm the numerical results of the original paper. 
+
+
+
+# References
---
+++ code/algorithms.pyx
@@ -0,0 +1,380 @@
+# -*- coding: utf-8 -*-
+#
+#    Copyright 2017: Frank Stollmeier
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+#cython: boundscheck=False
+cimport cython
+import numpy as np
+cimport numpy as np
+import networkx as nx
+import random
+from libc.stdlib cimport rand, srand
+cdef extern from "limits.h":
+    int INT_MAX
+
+
+
+# Functions to generate networkx-graphs
+
+
+def create_square_lattice_graph4(N1,N2, periodic = True):
+    '''Create a regular square lattice graph with N1*N2 nodes and k=4 edges per node.'''
+    g = nx.grid_graph(dim=[N1,N2],periodic=periodic)
+    return g
+
+
+def create_square_lattice_graph6(N1,N2, periodic = True):
+    '''Create a regular square lattice graph with N1*N2 nodes and k=6 edges per node.'''
+    g = nx.grid_graph(dim=[N1,N2],periodic=periodic)
+    for n in g.nodes():
+        g.add_edge(n,((n[0]+1)%N1,(n[1]+1)%N2))
+    return g
+
+
+def create_triangular_lattice_graph(N1,N2):
+    '''Create a regular triangular lettice graph with N1*N2 nodes and k=8 edges per node.'''
+    g = nx.grid_graph(dim=[N1,N2],periodic=True)
+    for i in range(N1):
+        for j in range(N2):
+            g.add_edge((i,j),((i+1)%N1,(j+1)%N2))
+            g.add_edge(((i+1)%N1,j),(i,(j+1)%N2))
+    return g
+
+
+def create_hexagonal_lattice_graph(N1,N2):
+    '''Create a regular hexagonal lattice graph with int(N1*N2*2/3.0) nodes and k=3 edges per node.
+    Example parameters:
+    N1=10 and N2=15 will produce a grid with N=100 nodes.
+    N1=25 and N2=30 will produce a grid with N=500 nodes. 
+    Note:
+    For certain parameters it happens that some nodes on the boundary of the grid have two or four neighbors. The function will print a warning in these cases.
+    '''
+    if N1<3 or N2<3 or N1%3==2 or N2%3==2:
+        #raise ValueError("These values are not allowed.")
+        print("Warning: With these values N1 and N2, there may be some nodes on the boundary of the lattice with 2 or 4 edges.\
+        If you prefer all nodes having exactly three edges, choose values which do not satisfy N1<3 or N2<3 or N1%3==2 or N2%3==2.")
+    g = create_square_lattice_graph6(N1,N2, periodic=True)
+    for i in range(N1):
+        for j in range(N2):
+            if (i+j)%3==0:
+                g.remove_node((i%N1,j%N2))
+            if i==0 and j%3==2:
+                g.remove_edge((i,j),(N1-1,j-1))
+            if i%3==2 and j==0:
+                u,v = (i,j), (i-1,N2-1)
+                if g.has_edge(u,v):
+                    g.remove_edge(u,v)
+    return g
+
+
+def create_connected_random_graph(N,k):
+    '''Create a connected random graph with N nodes and k edges per node on average.'''
+    #Algorithm: 
+    #First, add nodes and connect them to already connected nodes to ensure that the graph is connected.
+    #Second, add edges between randomly chosen pairs of nodes until the desired number of edges is present.
+    g = nx.Graph()
+    g.add_node(0)
+    nodes = [0]
+    for i in range(1,N):
+        g.add_node(i)
+        g.add_edge(i,random.choice(nodes))
+        nodes.append(i)
+    n_edges = np.array([len(g.edges(n)) for n in g.nodes()])
+    while np.mean(n_edges) <= k:
+        n1,n2 = random.randint(0,N-1), random.randint(0,N-1)
+        if n1 != n2:
+            g.add_edge(n1,n2)
+            n_edges[n1] = n_edges[n1] + 1
+            n_edges[n2] = n_edges[n2] + 1
+    return g
+
+
+def convert_networkx_graph_to_adjacency_list(G):
+    '''Convert a networkx graph G to an adjacency list stored in a numpy array of shape (N,N+1) 
+    in which row i stores the indices of the neighbors of node i followed by -1 values until the end of the row.
+    '''
+    adj_list = - np.ones((G.number_of_nodes(),G.number_of_nodes()+1),dtype=np.int)
+    index_dict = dict([(n,i) for i,n in enumerate(G.nodes_iter())])
+    for node in G.nodes_iter():
+        edges = G.edges(node)
+        adj_list[index_dict[node],:len(edges)] = [index_dict[e[1]] for e in edges]  
+    return adj_list
+
+
+
+
+
+# Function to measure fixation probabilities
+
+def measure_fixation_probabilities(graph_generator, graph_generator_arguments, w, c, n_graphs, n_runs, benefit_to_cost_ratios, dview = None, iteration_type = 1):
+    ''' For each value in benefit_to_cost_ratios, do n_graphs*n_runs simulations to measure the fixation probabilities.
+    
+    Parameters:
+    graph_generator: Function which returns a graph as an adjacency list. This adjacency list should be a numpy array of shape (N,N+1) in which row i stores the indices of the neighbors of node i followed by -1 values until the end of the row.
+    graph_generator_arguments: List of arguments passed to the graph_generator function.
+    w:  Selection strength.
+    c:  Costs.
+    n_graphs: The number of graphs generated with graph_generator.
+    n_runs: The number of simulations on each graph.
+    benefit_to_cost_ratios: List of b/c ratios.
+    dview (optional): ipyparallel.client.view.DirectView object for parallelization. The default value is None, meaning no parallelization.
+    iteration_type (optional): Either 1 (death-birth process) or 0 (imitation process). Default is 1. 
+    
+    Returns:
+    Fixation probabilities as a numpy-array of the same length as benefit_to_cost_ratios.
+    '''
+    fixation_probabilities = []
+    seeds = [rand() for i in xrange(n_graphs)]
+    for bc_ratio in benefit_to_cost_ratios:
+        b = c * bc_ratio
+        if dview is None:
+            results = [measure_fixation_probability(graph_generator(*graph_generator_arguments), n_runs, b, c, w, s, iteration_type) for s in seeds]
+        else:
+            dview.push(dict(graph_generator=graph_generator, graph_generator_arguments=graph_generator_arguments, n_runs=n_runs, b=b, c=c, w=w, iteration_type=iteration_type))
+            dview.scatter('seeds',seeds)
+            dview.execute('results = [measure_fixation_probability(graph_generator(*graph_generator_arguments), n_runs, b, c, w, s, iteration_type) for s in seeds]')
+            results = dview.gather('results')
+        fixation_probabilities.append(np.mean(results))
+    return np.array(fixation_probabilities) 
+
+
+def measure_fixation_probability(np.ndarray[np.int64_t, ndim=2] graph_nd, int n_runs, float b, float c, float w, int seed, int iteration_type):
+    '''Measure fixation probability by running many simulations starting with a random node as the initial cooperator. 
+    
+    Parameters:
+    graph_nd: The graph given as a numpy array of shape (N,N+1) in which row i stores the indices of the neighbors of node i followed by -1 values until the end of the row.
+    n_runs: Number of simulations.
+    b: Benefit value.
+    c: Cost value.
+    w: Selection strength.
+    seed: Seed for the random number generator.
+    iteration_type: Either 1 (death-birth process) or 0 (imitation process).
+    
+    Returns:
+    The ratio of the number of simulations that ended with a fixation of cooperators.
+    '''
+    srand(seed)
+    cdef long int [:,:] graph = graph_nd
+    cdef int N = graph.shape[0]
+    cdef int first_cooperator_index
+    cdef int C_fixations = 0
+    cdef int i = 0
+    for i in range(n_runs):
+        first_cooperator_index = int((rand() / (float(INT_MAX) + 1) * N))
+        if run_until_fixation(graph, N, first_cooperator_index, b, c, w, iteration_type):
+            C_fixations += 1
+    return C_fixations / float(n_runs)
+
+
+
+
+# Algorithms to simulate games on graphs
+
+cdef bint run_until_fixation(long int [:,:] graph, int N, int cooperator_index, float b, float c, float w, int iteration_type):
+    '''Run a simulation until all nodes have the same strategy. In the initial state all nodes defect except the one that is specified with cooperator_index.
+    
+    Parameters:
+    graph: The graph given as a numpy array of shape (N,N+1) in which row i stores the indices of the neighbors of node i followed by -1 values until the end of the row.
+    N: Size of the graph.
+    cooperator_index: Index of the initial cooperator.
+    b: Benefit value.
+    c: Cost value.
+    w: Selection strength.
+    iteration_type: Either 1 (death-birth process) or 0 (imitation process).
+    
+    Returns:
+    True or false, depending on whether the fixation of cooperators was successful.  
+    '''
+    cdef int i = 0
+    cdef int j = 0
+    
+    #Initialization
+    cdef long int [:] strategies = np.zeros(N, dtype=np.int)
+    strategies[cooperator_index] = 1
+    cdef int neighbors_index
+    cdef long int [:] adjacent_cooperators = np.zeros(N, dtype=np.int)
+    cdef long int [:] adjacent_defectors = np.zeros(N, dtype=np.int)
+    cdef int adj_cooperators
+    cdef int adj_defectors
+    for i in range(N):
+        adj_cooperators = 0
+        adj_defectors = 0
+        j = 0
+        neighbors_index = graph[i,j]
+        while neighbors_index >= 0:
+            if strategies[neighbors_index]:
+                adj_cooperators += 1
+            else:
+                adj_defectors += 1
+            j += 1
+            neighbors_index = graph[i,j]
+        adjacent_cooperators[i] = adj_cooperators
+        adjacent_defectors[i] = adj_defectors
+    
+    #Simulation
+    cdef int n_cooperators = 1
+    cdef int counter = 0
+    cdef int update_node_index
+    cdef double random_number
+    if iteration_type: #death-birth
+        iteration = iteration_DB
+    else:              #imitation
+        iteration = iteration_IM
+    while n_cooperators > 0 and n_cooperators < N:
+        update_node_index = int((rand() / (float(INT_MAX) + 1) * N))
+        random_number = rand() / float(INT_MAX)
+        n_cooperators += iteration(graph, strategies, adjacent_cooperators, adjacent_defectors, update_node_index, random_number, b, c, w)
+        counter += 1
+    return n_cooperators == N
+
+
+
+
+cdef int iteration_DB(long int [:,:] graph, long int [:] strategies, long int [:] adjacent_cooperators, long int [:] adjacent_defectors, int update_node_index, float random_number, float b, float c, float w) nogil:
+    '''Do one death-birth update.
+    
+    Parameters:
+    graph: The graph given as a numpy array of shape (N,N+1) in which row i stores the indices of the neighbors of node i followed by -1 values until the end of the row.
+    strategies: Integer array of shape (N) which stores the strategy of each node. If node i is a cooperator(defector), then strategies[i] is 1(0).
+    adjacent_cooperators: Integer array of shape (N) which stores the number of adjacent cooperators of each node.
+    adjacent_defectors: Integer array of shape (N) which stores the number of adjacent defectors of each node.
+    update_node_index: Index of the node where the death and the birth event happen.
+    random_number: A float value between 0 and 1.
+    b: Benefit value.
+    c: Cost value.
+    w: Selection strength.
+        
+    Returns:
+    -1, 0 or +1, depending on whether the number of cooperators decreased by one, stayed constant, or increased by one.
+    '''
+    cdef int previous_strategy = strategies[update_node_index]
+    
+    #Sum up fitness of adjacent neighbors
+    cdef float F_C = 0
+    cdef float F_D = 0
+    cdef float f
+    cdef int neighbors_index = graph[update_node_index,0]
+    cdef int i = 0
+    while neighbors_index >= 0:
+        f = fitness(strategies[neighbors_index], adjacent_cooperators[neighbors_index], adjacent_defectors[neighbors_index], b, c, w)
+        if strategies[neighbors_index]:
+            F_C += f
+        else:
+            F_D += f
+        i += 1
+        neighbors_index = graph[update_node_index,i]
+    
+    #Birth step
+    cdef int diff_n_cooperators = 0
+    if random_number < F_C / (F_C+F_D):
+        strategies[update_node_index] = 1
+        if not previous_strategy:
+            diff_n_cooperators = +1
+    else:
+        strategies[update_node_index] = 0
+        if previous_strategy:
+            diff_n_cooperators = -1
+    
+    #Update the stored information about the neighbors of each node
+    if diff_n_cooperators != 0:
+        i = 0
+        while True:
+            neighbors_index = graph[update_node_index,i]
+            if neighbors_index < 0:
+                break
+            adjacent_cooperators[neighbors_index] += diff_n_cooperators
+            adjacent_defectors[neighbors_index] -= diff_n_cooperators
+            i += 1
+    
+    return diff_n_cooperators
+
+
+cdef int iteration_IM(long int [:,:] graph, long int [:] strategies, long int [:] adjacent_cooperators, long int [:] adjacent_defectors, int update_node_index, float random_number, float b, float c, float w) nogil:
+    '''Do one imitation step.
+    
+    Parameters:
+    graph: The graph given as a numpy array of shape (N,N+1) in which row i stores the indices of the neighbors of node i followed by -1 values until the end of the row.
+    strategies: Integer array of shape (N) which stores the strategy of each node. If node i is a cooperator(defector), then strategies[i] is 1(0).
+    adjacent_cooperators: Integer array of shape (N) which stores the number of adjacent cooperators of each node.
+    adjacent_defectors: Integer array of shape (N) which stores the number of adjacent defectors of each node.
+    update_node_index: Index of the node where the death and the birth event happen.
+    random_number: A float value between 0 and 1.
+    b: Benefit value.
+    c: Cost value.
+    w: Selection strength.
+        
+    Returns:
+    -1, 0 or +1, depending on whether the number of cooperators decreased by one, stayed constant, or increased by one.
+    '''
+    cdef int previous_strategy = strategies[update_node_index]
+    
+    #Sum up fitness of adjacent neighbors
+    cdef float F_C = 0
+    cdef float F_D = 0
+    cdef float f
+    cdef int neighbors_index = graph[update_node_index,0]
+    cdef int i = 0
+    while neighbors_index >= 0:
+        f = fitness(strategies[neighbors_index], adjacent_cooperators[neighbors_index], adjacent_defectors[neighbors_index], b, c, w)
+        if strategies[neighbors_index]:
+            F_C += f
+        else:
+            F_D += f
+        i += 1
+        neighbors_index = graph[update_node_index,i]
+    
+    #Imitation
+    cdef int diff_n_cooperators = 0
+    cdef float f0 = fitness(strategies[update_node_index], adjacent_cooperators[update_node_index], adjacent_defectors[update_node_index], b, c, w)
+    if not previous_strategy and random_number < F_C / (F_C+F_D+f0):
+        strategies[update_node_index] = 1
+        diff_n_cooperators = +1
+    elif previous_strategy and random_number < F_D / (F_C+F_D+f0):
+        strategies[update_node_index] = 0
+        diff_n_cooperators = -1
+    
+    #Update the stored information about the neighbors of each node
+    if diff_n_cooperators != 0:
+        i = 0
+        while True:
+            neighbors_index = graph[update_node_index,i]
+            if neighbors_index < 0:
+                break
+            adjacent_cooperators[neighbors_index] += diff_n_cooperators
+            adjacent_defectors[neighbors_index] -= diff_n_cooperators
+            i += 1
+    return diff_n_cooperators
+
+
+cdef inline float fitness(int strategy, int n_cooperators, int n_defectors, float b, float c, float w) nogil:
+    '''Calculate the fitness of a node. 
+    
+    Parameters:
+    strategy: Integer value, 1 (0) if the node is a cooperator (defector).
+    n_cooperators: The number of adjacent nodes that cooperate.
+    n_defectors: The number of adjacent nodes that defect.
+    b: Benefit value.
+    c: Cost value.
+    w: Selection strength.
+    '''
+    cdef float payoff
+    if strategy:
+        payoff = b*n_cooperators - c*(n_cooperators + n_defectors)
+    else:
+        payoff = b*n_cooperators
+    return 1 - w + w*payoff
+

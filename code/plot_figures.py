ba251c5aa025cce4d34f5e6e0b620da38a57af22
ChristophMetzner <metz@gmx.li>
2017-05-17 13:54:55 +0100

    enlarged label and ticklabel sizes

--- code/plot_figures.py
+++ code/plot_figures.py
@@ -9,18 +9,9 @@ def calcPowerSpectrum(meg,dt,sim_time):
   	fs = 1. / dt	
 
 	tn = np.linspace(0,sim_time,int(sim_time/dt))
-
   	npts = len(meg)
- 	startpt = int(0.2*fs)
 
-  	if (npts - startpt)%2!=0:
-      		startpt = startpt + 1
-
-  	meg = meg[startpt:]
-  	tn = tn[startpt:]
-  	nfft = len(tn)
-
-  	pxx,freqs=mlab.psd(meg,NFFT=nfft,Fs=fs,noverlap=0,window=mlab.window_none)
+  	pxx,freqs=mlab.psd(meg,NFFT=npts,Fs=fs,noverlap=0,window=mlab.window_none)
   	pxx[0] = 0.0
 	
 	return pxx,freqs
@@ -75,40 +66,45 @@ showfig = 1	# set plotfi to 1 if you want to show the figures
 
 ########### Figure 1 - Replication of Figure 4 from Vierling-Claassen et al.
 #plt.figure(1)
-f,((ax1,ax2),(ax3,ax4),(ax5,ax6)) = plt.subplots(3,2,sharex=True,sharey=True,figsize=[15.0,15.0])
+f,((ax1,ax2),(ax3,ax4),(ax5,ax6)) = plt.subplots(3,2,sharex=True,sharey=True,figsize=[19.0,15.0])
 
 ax1.plot(time,ctrl_avg_meg_40,'k',linewidth=1.5)
-ax1.set_xlabel('Time (ms)')
-ax1.set_ylabel('Simulated MEG')
-ax1.annotate('40 Hz \n Drive',xy=(0,0.5),xytext=(-ax1.yaxis.labelpad-5,0),xycoords=ax1.yaxis.label,textcoords='offset points',size=25,ha='right',va='center')
+ax1.set_xlabel('Time (ms)',fontsize=18)
+ax1.set_ylabel('Simulated MEG',fontsize=18)
+ax1.annotate('40 Hz \n Drive',xy=(0,0.5),xytext=(-ax1.yaxis.labelpad-5,0),xycoords=ax1.yaxis.label,textcoords='offset points',size=35,ha='right',va='center')
 ax2.plot(time,schiz_avg_meg_40,'k',linewidth=1.5)
-ax2.set_xlabel('Time (ms)')
-ax2.set_ylabel('Simulated MEG')
+ax2.set_xlabel('Time (ms)',fontsize=18)
+ax2.set_ylabel('Simulated MEG',fontsize=18)
 ax3.plot(time,ctrl_avg_meg_30,'k',linewidth=1.5)
-ax3.set_xlabel('Time (ms)')
-ax3.set_ylabel('Simulated MEG')
-ax3.annotate('30 Hz \n Drive',xy=(0,0.5),xytext=(-ax3.yaxis.labelpad-5,0),xycoords=ax3.yaxis.label,textcoords='offset points',size=25,ha='right',va='center')
+ax3.set_xlabel('Time (ms)',fontsize=18)
+ax3.set_ylabel('Simulated MEG',fontsize=18)
+ax3.annotate('30 Hz \n Drive',xy=(0,0.5),xytext=(-ax3.yaxis.labelpad-5,0),xycoords=ax3.yaxis.label,textcoords='offset points',size=35,ha='right',va='center')
 ax4.plot(time,schiz_avg_meg_30,'k',linewidth=1.5)
-ax4.set_xlabel('Time (ms)')
-ax4.set_ylabel('Simulated MEG')
+ax4.set_xlabel('Time (ms)',fontsize=18)
+ax4.set_ylabel('Simulated MEG',fontsize=18)
 ax5.plot(time,ctrl_avg_meg_20,'k',linewidth=1.5)
-ax5.set_xlabel('Time (ms)')
-ax5.set_ylabel('Simulated MEG')
-ax5.annotate('20 Hz \n Drive',xy=(0,0.5),xytext=(-ax5.yaxis.labelpad-5,0),xycoords=ax5.yaxis.label,textcoords='offset points',size=25,ha='right',va='center')
-ax5.annotate('Control',xy=(0.5,0),xytext=(0,-75),xycoords='axes fraction',textcoords='offset points',size=25,ha='center',va='bottom')
+ax5.set_xlabel('Time (ms)',fontsize=18)
+ax5.set_ylabel('Simulated MEG',fontsize=18)
+ax5.annotate('20 Hz \n Drive',xy=(0,0.5),xytext=(-ax5.yaxis.labelpad-5,0),xycoords=ax5.yaxis.label,textcoords='offset points',size=35,ha='right',va='center')
+ax5.annotate('Control',xy=(0.5,0),xytext=(0,-75),xycoords='axes fraction',textcoords='offset points',size=35,ha='center',va='bottom')
 ax6.plot(time,schiz_avg_meg_20,'k',linewidth=1.5)
-ax6.set_xlabel('Time (ms)')
-ax6.set_ylabel('Simulated MEG')
-ax6.annotate('Schizophrenia',xy=(0.5,0),xytext=(0,-75),xycoords='axes fraction',textcoords='offset points',size=25,ha='center',va='bottom')
+ax6.set_xlabel('Time (ms)',fontsize=18)
+ax6.set_ylabel('Simulated MEG',fontsize=18)
+ax6.annotate('Schizophrenia',xy=(0.5,0),xytext=(0,-75),xycoords='axes fraction',textcoords='offset points',size=35,ha='center',va='bottom')
 
-plt.setp(ax1.get_xticklabels(),visible=True)
-plt.setp(ax2.get_xticklabels(),visible=True)
-plt.setp(ax3.get_xticklabels(),visible=True)
-plt.setp(ax4.get_xticklabels(),visible=True)
+plt.setp(ax1.get_xticklabels(),visible=True,fontsize=18)
+plt.setp(ax2.get_xticklabels(),visible=True,fontsize=18)
+plt.setp(ax3.get_xticklabels(),visible=True,fontsize=18)
+plt.setp(ax4.get_xticklabels(),visible=True,fontsize=18)
+plt.setp(ax5.get_xticklabels(),visible=True,fontsize=18)
+plt.setp(ax6.get_xticklabels(),visible=True,fontsize=18)
 
-plt.setp(ax2.get_yticklabels(),visible=True)
-plt.setp(ax4.get_yticklabels(),visible=True)
-plt.setp(ax6.get_yticklabels(),visible=True)
+plt.setp(ax2.get_yticklabels(),visible=True,fontsize=18)
+plt.setp(ax4.get_yticklabels(),visible=True,fontsize=18)
+plt.setp(ax6.get_yticklabels(),visible=True,fontsize=18)
+plt.setp(ax1.get_yticklabels(),visible=True,fontsize=18)
+plt.setp(ax3.get_yticklabels(),visible=True,fontsize=18)
+plt.setp(ax5.get_yticklabels(),visible=True,fontsize=18)
 
 if savefig:
 	plt.savefig('../data/Figures/Replication-Figure4.png',dpi=600)
@@ -126,71 +122,76 @@ pxx_schiz_40,_ = calcPowerSpectrum(schiz_avg_meg_40,dt,sim_time)
 
 # Plot figure
 #plt.figure(2)
-f2,((ax11,ax22),(ax33,ax44),(ax55,ax66)) = plt.subplots(3,2,sharex=True,sharey=True,figsize=[15.0,15.0])
+f2,((ax11,ax22),(ax33,ax44),(ax55,ax66)) = plt.subplots(3,2,sharex=True,sharey=True,figsize=[19.0,15.0])
 
 ax11.plot(freqs*1000,pxx_ctrl_40,'k',linewidth=2)
 ax11.axis(xmin=0, xmax=55)
-ax11.set_xlabel('Frequency (Hz)')
-ax11.set_ylabel('Power')
-ax11.annotate('40 Hz \n Drive',xy=(0,0.5),xytext=(-ax11.yaxis.labelpad-5,0),xycoords=ax11.yaxis.label,textcoords='offset points',size=25,ha='right',va='center')
+ax11.set_xlabel('Frequency (Hz)',fontsize=18)
+ax11.set_ylabel('Power',fontsize=18)
+ax11.annotate('40 Hz \n Drive',xy=(0,0.5),xytext=(-ax11.yaxis.labelpad-5,0),xycoords=ax11.yaxis.label,textcoords='offset points',size=35,ha='right',va='center')
 xticks=ax11.xaxis.get_ticklabels()
 xticks[2].set_weight('bold')
 xticks[3].set_weight('bold')
 xticks[4].set_weight('bold')
 
 ax22.plot(freqs*1000,pxx_schiz_40,'k',linewidth=2)
-ax22.set_xlabel('Frequency (Hz)')
-ax22.set_ylabel('Power')
+ax22.set_xlabel('Frequency (Hz)',fontsize=18)
+ax22.set_ylabel('Power',fontsize=18)
 xticks=ax22.xaxis.get_ticklabels()
 xticks[2].set_weight('bold')
 xticks[3].set_weight('bold')
 xticks[4].set_weight('bold')
 
 ax33.plot(freqs*1000,pxx_ctrl_30,'k',linewidth=2)
-ax33.set_xlabel('Frequency (Hz)')
-ax33.set_ylabel('Power')
-ax33.annotate('30 Hz \n Drive',xy=(0,0.5),xytext=(-ax33.yaxis.labelpad-5,0),xycoords=ax33.yaxis.label,textcoords='offset points',size=25,ha='right',va='center')
+ax33.set_xlabel('Frequency (Hz)',fontsize=18)
+ax33.set_ylabel('Power',fontsize=18)
+ax33.annotate('30 Hz \n Drive',xy=(0,0.5),xytext=(-ax33.yaxis.labelpad-5,0),xycoords=ax33.yaxis.label,textcoords='offset points',size=35,ha='right',va='center')
 xticks=ax33.xaxis.get_ticklabels()
 xticks[2].set_weight('bold')
 xticks[3].set_weight('bold')
 xticks[4].set_weight('bold')
 
 ax44.plot(freqs*1000,pxx_schiz_30,'k',linewidth=2)
-ax44.set_xlabel('Frequency (Hz)')
-ax44.set_ylabel('Power')
+ax44.set_xlabel('Frequency (Hz)',fontsize=18)
+ax44.set_ylabel('Power',fontsize=18)
 xticks=ax44.xaxis.get_ticklabels()
 xticks[2].set_weight('bold')
 xticks[3].set_weight('bold')
 xticks[4].set_weight('bold')
 
 ax55.plot(freqs*1000,pxx_ctrl_20,'k',linewidth=2)
-ax55.set_xlabel('Frequency (Hz)')
-ax55.set_ylabel('Power')
-ax55.annotate('20 Hz \n Drive',xy=(0,0.5),xytext=(-ax55.yaxis.labelpad-5,0),xycoords=ax55.yaxis.label,textcoords='offset points',size=25,ha='right',va='center')
-ax55.annotate('Control',xy=(0.5,0),xytext=(0,-75),xycoords='axes fraction',textcoords='offset points',size=25,ha='center',va='bottom')
+ax55.set_xlabel('Frequency (Hz)',fontsize=18)
+ax55.set_ylabel('Power',fontsize=18)
+ax55.annotate('20 Hz \n Drive',xy=(0,0.5),xytext=(-ax55.yaxis.labelpad-5,0),xycoords=ax55.yaxis.label,textcoords='offset points',size=35,ha='right',va='center')
+ax55.annotate('Control',xy=(0.5,0),xytext=(0,-75),xycoords='axes fraction',textcoords='offset points',size=35,ha='center',va='bottom')
 xticks=ax55.xaxis.get_ticklabels()
 xticks[2].set_weight('bold')
 xticks[3].set_weight('bold')
 xticks[4].set_weight('bold')
 
 ax66.plot(freqs*1000,pxx_schiz_20,'k',linewidth=2)
-ax66.set_xlabel('Frequency (Hz)')
-ax66.set_ylabel('Power')
-ax66.annotate('Schizophrenia',xy=(0.5,0),xytext=(0,-75),xycoords='axes fraction',textcoords='offset points',size=25,ha='center',va='bottom')
+ax66.set_xlabel('Frequency (Hz)',fontsize=18)
+ax66.set_ylabel('Power',fontsize=18)
+ax66.annotate('Schizophrenia',xy=(0.5,0),xytext=(0,-75),xycoords='axes fraction',textcoords='offset points',size=35,ha='center',va='bottom')
 xticks=ax66.xaxis.get_ticklabels()
 xticks[2].set_weight('bold')
 xticks[3].set_weight('bold')
 xticks[4].set_weight('bold')
 
 
-plt.setp(ax11.get_xticklabels(),visible=True)
-plt.setp(ax22.get_xticklabels(),visible=True)
-plt.setp(ax33.get_xticklabels(),visible=True)
-plt.setp(ax44.get_xticklabels(),visible=True)
+plt.setp(ax11.get_xticklabels(),visible=True,fontsize=18)
+plt.setp(ax22.get_xticklabels(),visible=True,fontsize=18)
+plt.setp(ax33.get_xticklabels(),visible=True,fontsize=18)
+plt.setp(ax44.get_xticklabels(),visible=True,fontsize=18)
+plt.setp(ax55.get_xticklabels(),visible=True,fontsize=18)
+plt.setp(ax66.get_xticklabels(),visible=True,fontsize=18)
 
-plt.setp(ax22.get_yticklabels(),visible=True)
-plt.setp(ax44.get_yticklabels(),visible=True)
-plt.setp(ax66.get_yticklabels(),visible=True)
+plt.setp(ax22.get_yticklabels(),visible=True,fontsize=18)
+plt.setp(ax44.get_yticklabels(),visible=True,fontsize=18)
+plt.setp(ax66.get_yticklabels(),visible=True,fontsize=18)
+plt.setp(ax11.get_yticklabels(),visible=True,fontsize=18)
+plt.setp(ax33.get_yticklabels(),visible=True,fontsize=18)
+plt.setp(ax55.get_yticklabels(),visible=True,fontsize=18)
 
 if savefig:
 	plt.savefig('../data/Figures/Replication-Figure5.png',dpi=600)
@@ -221,24 +222,28 @@ for i,times in enumerate(spike_times):
 		y = [i]*len(times)
 		ax2.plot(times,y,linestyle='None',color='b',marker='|',markersize=10)
 		ax2.axis([0,sim_time,-0.5,30])
-ax2.set_xlabel('Time (ms)')
+ax2.set_xlabel('Time (ms)',fontsize=18)
 ax2.set_yticks([])
-ax2.annotate('I cells',xy=(0,0.5),xytext=(-ax2.yaxis.labelpad,-75),xycoords=ax2.yaxis.label,textcoords='offset points',size=12,ha='right',va='top')
-ax2.annotate('E cells',xy=(0,0.5),xytext=(-ax2.yaxis.labelpad,75),xycoords=ax2.yaxis.label,textcoords='offset points',size=12,ha='right',va='bottom')
+ax2.annotate('I cells',xy=(0,0.5),xytext=(-ax2.yaxis.labelpad,-75),xycoords=ax2.yaxis.label,textcoords='offset points',size=20,ha='right',va='top')
+ax2.annotate('E cells',xy=(0,0.5),xytext=(-ax2.yaxis.labelpad,75),xycoords=ax2.yaxis.label,textcoords='offset points',size=20,ha='right',va='bottom')
+plt.setp(ax2.get_xticklabels(),fontsize=18)
 
 # MEG plot
 ax3.plot(time,data_meg,'k',linewidth=1.5)
-ax3.set_xlabel('Time (ms)')
-ax3.set_ylabel('Simulated MEG')
-
+ax3.set_xlabel('Time (ms)',fontsize=20)
+ax3.set_ylabel('Simulated MEG',fontsize=20)
+plt.setp(ax3.get_xticklabels(),fontsize=18)
+plt.setp(ax3.get_yticklabels(),fontsize=18)
 
 # Power spectrum plot
 pxx,freqs = calcPowerSpectrum(data_meg,dt,sim_time)
 
 ax1.plot(freqs*1000,pxx,'k',linewidth=2)
 ax1.axis(xmin=0, xmax=55)
-ax1.set_xlabel('Frequency (Hz)')
-ax1.set_ylabel('Power')
+ax1.set_xlabel('Frequency (Hz)',fontsize=20)
+ax1.set_ylabel('Power',fontsize=20)
+plt.setp(ax1.get_xticklabels(),fontsize=18)
+plt.setp(ax1.get_yticklabels(),fontsize=18)
 
 if savefig:
 	plt.savefig('../data/Figures/Replication-Figure6.png',dpi=600)
@@ -269,24 +274,29 @@ for i,times in enumerate(spike_times):
 		y = [i]*len(times)
 		ax2.plot(times,y,linestyle='None',color='b',marker='|',markersize=10)
 		ax2.axis([0,sim_time,-0.5,30])
-ax2.set_xlabel('Time (ms)')
+ax2.set_xlabel('Time (ms)',fontsize=20)
 ax2.set_yticks([])
-ax2.annotate('I cells',xy=(0,0.5),xytext=(-ax2.yaxis.labelpad,-75),xycoords=ax2.yaxis.label,textcoords='offset points',size=12,ha='right',va='top')
-ax2.annotate('E cells',xy=(0,0.5),xytext=(-ax2.yaxis.labelpad,75),xycoords=ax2.yaxis.label,textcoords='offset points',size=12,ha='right',va='bottom')
+ax2.annotate('I cells',xy=(0,0.5),xytext=(-ax2.yaxis.labelpad,-75),xycoords=ax2.yaxis.label,textcoords='offset points',size=20,ha='right',va='top')
+ax2.annotate('E cells',xy=(0,0.5),xytext=(-ax2.yaxis.labelpad,75),xycoords=ax2.yaxis.label,textcoords='offset points',size=20,ha='right',va='bottom')
+plt.setp(ax2.get_xticklabels(),fontsize=18)
+
 
 # MEG plot
 ax3.plot(time,data_meg,'k',linewidth=1.5)
-ax3.set_xlabel('Time (ms)')
-ax3.set_ylabel('Simulated MEG')
-
+ax3.set_xlabel('Time (ms)',fontsize=20)
+ax3.set_ylabel('Simulated MEG',fontsize=20)
+plt.setp(ax3.get_xticklabels(),fontsize=18)
+plt.setp(ax3.get_yticklabels(),fontsize=18)
 
 # Power spectrum plot
 pxx,freqs = calcPowerSpectrum(data_meg,dt,sim_time)
 
 ax1.plot(freqs*1000,pxx,'k',linewidth=2)
 ax1.axis(xmin=0, xmax=55)
-ax1.set_xlabel('Frequency (Hz)')
-ax1.set_ylabel('Power')
+ax1.set_xlabel('Frequency (Hz)',fontsize=20)
+ax1.set_ylabel('Power',fontsize=20)
+plt.setp(ax1.get_xticklabels(),fontsize=18)
+plt.setp(ax1.get_yticklabels(),fontsize=18)
 
 if savefig:
 	plt.savefig('../data/Figures/Replication-Figure7.png',dpi=600)
@@ -317,24 +327,28 @@ for i,times in enumerate(spike_times):
 		y = [i]*len(times)
 		ax2.plot(times,y,linestyle='None',color='b',marker='|',markersize=10)
 		ax2.axis([0,sim_time,-0.5,30])
-ax2.set_xlabel('Time (ms)')
+ax2.set_xlabel('Time (ms)',fontsize=20)
 ax2.set_yticks([])
-ax2.annotate('I cells',xy=(0,0.5),xytext=(-ax2.yaxis.labelpad,-75),xycoords=ax2.yaxis.label,textcoords='offset points',size=12,ha='right',va='top')
-ax2.annotate('E cells',xy=(0,0.5),xytext=(-ax2.yaxis.labelpad,75),xycoords=ax2.yaxis.label,textcoords='offset points',size=12,ha='right',va='bottom')
+ax2.annotate('I cells',xy=(0,0.5),xytext=(-ax2.yaxis.labelpad,-75),xycoords=ax2.yaxis.label,textcoords='offset points',size=20,ha='right',va='top')
+ax2.annotate('E cells',xy=(0,0.5),xytext=(-ax2.yaxis.labelpad,75),xycoords=ax2.yaxis.label,textcoords='offset points',size=20,ha='right',va='bottom')
+plt.setp(ax2.get_xticklabels(),fontsize=18)
 
 # MEG plot
 ax3.plot(time,data_meg,'k',linewidth=1.5)
-ax3.set_xlabel('Time (ms)')
-ax3.set_ylabel('Simulated MEG')
-
+ax3.set_xlabel('Time (ms)',fontsize=20)
+ax3.set_ylabel('Simulated MEG',fontsize=20)
+plt.setp(ax3.get_xticklabels(),fontsize=18)
+plt.setp(ax3.get_yticklabels(),fontsize=18)
 
 # Power spectrum plot
 pxx,freqs = calcPowerSpectrum(data_meg,dt,sim_time)
 
 ax1.plot(freqs*1000,pxx,'k',linewidth=2)
 ax1.axis(xmin=0, xmax=55)
-ax1.set_xlabel('Frequency (Hz)')
-ax1.set_ylabel('Power')
+ax1.set_xlabel('Frequency (Hz)',fontsize=20)
+ax1.set_ylabel('Power',fontsize=20)
+plt.setp(ax1.get_xticklabels(),fontsize=18)
+plt.setp(ax1.get_yticklabels(),fontsize=18)
 
 if savefig:
 	plt.savefig('../data/Figures/Replication-Figure10.png',dpi=600)
@@ -365,24 +379,28 @@ for i,times in enumerate(spike_times):
 		y = [i]*len(times)
 		ax2.plot(times,y,linestyle='None',color='b',marker='|',markersize=10)
 		ax2.axis([0,sim_time,-0.5,30])
-ax2.set_xlabel('Time (ms)')
+ax2.set_xlabel('Time (ms)',fontsize=20)
 ax2.set_yticks([])
-ax2.annotate('I cells',xy=(0,0.5),xytext=(-ax2.yaxis.labelpad,-75),xycoords=ax2.yaxis.label,textcoords='offset points',size=12,ha='right',va='top')
-ax2.annotate('E cells',xy=(0,0.5),xytext=(-ax2.yaxis.labelpad,75),xycoords=ax2.yaxis.label,textcoords='offset points',size=12,ha='right',va='bottom')
+ax2.annotate('I cells',xy=(0,0.5),xytext=(-ax2.yaxis.labelpad,-75),xycoords=ax2.yaxis.label,textcoords='offset points',size=20,ha='right',va='top')
+ax2.annotate('E cells',xy=(0,0.5),xytext=(-ax2.yaxis.labelpad,75),xycoords=ax2.yaxis.label,textcoords='offset points',size=20,ha='right',va='bottom')
+plt.setp(ax2.get_xticklabels(),fontsize=18)
 
 # MEG plot
 ax3.plot(time,data_meg,'k',linewidth=1.5)
-ax3.set_xlabel('Time (ms)')
-ax3.set_ylabel('Simulated MEG')
-
+ax3.set_xlabel('Time (ms)',fontsize=20)
+ax3.set_ylabel('Simulated MEG',fontsize=20)
+plt.setp(ax3.get_xticklabels(),fontsize=18)
+plt.setp(ax3.get_yticklabels(),fontsize=18)
 
 # Power spectrum plot
 pxx,freqs = calcPowerSpectrum(data_meg,dt,sim_time)
 
 ax1.plot(freqs*1000,pxx,'k',linewidth=2)
 ax1.axis(xmin=0, xmax=55)
-ax1.set_xlabel('Frequency (Hz)')
-ax1.set_ylabel('Power')
+ax1.set_xlabel('Frequency (Hz)',fontsize=20)
+ax1.set_ylabel('Power',fontsize=20)
+plt.setp(ax1.get_xticklabels(),fontsize=18)
+plt.setp(ax1.get_yticklabels(),fontsize=18)
 
 if savefig:
 	plt.savefig('../data/Figures/Replication-Figure11.png',dpi=600)

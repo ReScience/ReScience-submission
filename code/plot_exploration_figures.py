8def7f22620dcf96ef6cc1cc40f5c485c168c902
ChristophMetzner <metz@gmx.li>
2017-05-23 13:08:45 +0100

    uodate exploration figures

--- code/plot_exploration_figures.py
+++ code/plot_exploration_figures.py
@@ -51,10 +51,10 @@ time = np.linspace(0,sim_time,int(sim_time/dt))
 
 
 # Flags
-savefig = 0	# set savefig to 1 if you want to store the figures
+savefig = 1	# set savefig to 1 if you want to store the figures
 showfig = 1	# set plotfi to 1 if you want to show the figures
 
-########### Figure 7 - Exploration of the influence of noise strength
+########### Figure 7 - Exploration of the influence of noise strength (raw MEG)
 # load data
 ctrl_avg_meg_20_A_0 = np.load('../data/Data-Average/sims_ctrl_avg_20Hz_A0.0.npy')
 schiz_avg_meg_40_A_0 = np.load('../data/Data-Average/sims_schiz_avg_40Hz_A0.0.npy')
@@ -104,6 +104,95 @@ plt.setp(ax6.get_yticklabels(),visible=True,fontsize=18)
 plt.setp(ax1.get_yticklabels(),visible=True,fontsize=18)
 plt.setp(ax3.get_yticklabels(),visible=True,fontsize=18)
 plt.setp(ax5.get_yticklabels(),visible=True,fontsize=18)
+
+if savefig:
+	plt.savefig('../data/Figures/Noise-Exploration.png',dpi=600)
+	plt.savefig('../data/Figures/Noise-Exploration.eps',dpi=600)
+
+########### Figure 8 - Exploration of the influence of noise strength (PSD)
+# load data
+pxx_ctrl_20_A_0,freqs = calcPowerSpectrum(ctrl_avg_meg_20_A_0,dt,sim_time)
+pxx_schiz_40_A_0,_     = calcPowerSpectrum(schiz_avg_meg_40_A_0,dt,sim_time)
+
+pxx_ctrl_20_A_0_25,freqs = calcPowerSpectrum(ctrl_avg_meg_20_A_0_25,dt,sim_time)
+pxx_schiz_40_A_0_25,_     = calcPowerSpectrum(schiz_avg_meg_40_A_0_25,dt,sim_time)
+
+pxx_ctrl_20_A_0_375,freqs = calcPowerSpectrum(ctrl_avg_meg_20_A_0_375,dt,sim_time)
+pxx_schiz_40_A_0_375,_     = calcPowerSpectrum(schiz_avg_meg_40_A_0_375,dt,sim_time)
+
+# plot
+# Plot figure
+#plt.figure(2)
+f2,((ax11,ax22),(ax33,ax44),(ax55,ax66)) = plt.subplots(3,2,sharex=True,sharey=True,figsize=[19.0,15.0])
+
+ax11.plot(freqs*1000,pxx_schiz_40_A_0,'k',linewidth=2)
+ax11.axis(xmin=0, xmax=55)
+ax11.set_xlabel('Frequency (Hz)',fontsize=18)
+ax11.set_ylabel('Power',fontsize=18)
+ax11.annotate('0% \n Noise ',xy=(0,0.5),xytext=(-ax11.yaxis.labelpad-5,0),xycoords=ax11.yaxis.label,textcoords='offset points',size=35,ha='right',va='center')
+xticks=ax11.xaxis.get_ticklabels()
+xticks[2].set_weight('bold')
+xticks[3].set_weight('bold')
+xticks[4].set_weight('bold')
+
+ax22.plot(freqs*1000,pxx_ctrl_20_A_0,'k',linewidth=2)
+ax22.set_xlabel('Frequency (Hz)',fontsize=18)
+ax22.set_ylabel('Power',fontsize=18)
+xticks=ax22.xaxis.get_ticklabels()
+xticks[2].set_weight('bold')
+xticks[3].set_weight('bold')
+xticks[4].set_weight('bold')
+
+ax33.plot(freqs*1000,pxx_schiz_40_A_0_25,'k',linewidth=2)
+ax33.set_xlabel('Frequency (Hz)',fontsize=18)
+ax33.set_ylabel('Power',fontsize=18)
+ax33.annotate('50% \n Noise ',xy=(0,0.5),xytext=(-ax33.yaxis.labelpad-5,0),xycoords=ax33.yaxis.label,textcoords='offset points',size=35,ha='right',va='center')
+xticks=ax33.xaxis.get_ticklabels()
+xticks[2].set_weight('bold')
+xticks[3].set_weight('bold')
+xticks[4].set_weight('bold')
+
+ax44.plot(freqs*1000,pxx_ctrl_20_A_0_25,'k',linewidth=2)
+ax44.set_xlabel('Frequency (Hz)',fontsize=18)
+ax44.set_ylabel('Power',fontsize=18)
+xticks=ax44.xaxis.get_ticklabels()
+xticks[2].set_weight('bold')
+xticks[3].set_weight('bold')
+xticks[4].set_weight('bold')
+
+ax55.plot(freqs*1000,pxx_schiz_40_A_0_375,'k',linewidth=2)
+ax55.set_xlabel('Frequency (Hz)',fontsize=18)
+ax55.set_ylabel('Power',fontsize=18)
+ax55.annotate('75% \n Noise ',xy=(0,0.5),xytext=(-ax55.yaxis.labelpad-5,0),xycoords=ax55.yaxis.label,textcoords='offset points',size=35,ha='right',va='center')
+ax55.annotate('Schizophrenia 40Hz Drive',xy=(0.5,0),xytext=(0,-75),xycoords='axes fraction',textcoords='offset points',size=35,ha='center',va='bottom')
+xticks=ax55.xaxis.get_ticklabels()
+xticks[2].set_weight('bold')
+xticks[3].set_weight('bold')
+xticks[4].set_weight('bold')
+
+ax66.plot(freqs*1000,pxx_ctrl_20_A_0_375,'k',linewidth=2)
+ax66.set_xlabel('Frequency (Hz)',fontsize=18)
+ax66.set_ylabel('Power',fontsize=18)
+ax66.annotate('Control 29 Hz Drive',xy=(0.5,0),xytext=(0,-75),xycoords='axes fraction',textcoords='offset points',size=35,ha='center',va='bottom')
+xticks=ax66.xaxis.get_ticklabels()
+xticks[2].set_weight('bold')
+xticks[3].set_weight('bold')
+xticks[4].set_weight('bold')
+
+
+plt.setp(ax11.get_xticklabels(),visible=True,fontsize=18)
+plt.setp(ax22.get_xticklabels(),visible=True,fontsize=18)
+plt.setp(ax33.get_xticklabels(),visible=True,fontsize=18)
+plt.setp(ax44.get_xticklabels(),visible=True,fontsize=18)
+plt.setp(ax55.get_xticklabels(),visible=True,fontsize=18)
+plt.setp(ax66.get_xticklabels(),visible=True,fontsize=18)
+
+plt.setp(ax22.get_yticklabels(),visible=True,fontsize=18)
+plt.setp(ax44.get_yticklabels(),visible=True,fontsize=18)
+plt.setp(ax66.get_yticklabels(),visible=True,fontsize=18)
+plt.setp(ax11.get_yticklabels(),visible=True,fontsize=18)
+plt.setp(ax33.get_yticklabels(),visible=True,fontsize=18)
+plt.setp(ax55.get_yticklabels(),visible=True,fontsize=18)
 
 if savefig:
 	plt.savefig('../data/Figures/Noise-Exploration.png',dpi=600)

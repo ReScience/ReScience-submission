886c709ed623344fc55896de1bf37d7f34ba47f0
Timothée Poisot <timothee.poisot@umontreal.ca>
2017-11-03 11:21:35 -0400

    Fix y-axis in figure 3

--- code/figure04.jl
+++ code/figure04.jl
@@ -25,6 +25,6 @@ plot(out[:,1], out[:,2], m=:utriangle, c=:black, lab="Specialist 1")
 plot!(out[:,1], out[:,3], m=:dtriangle, c=:black, lab="Specialist 2")
 plot!(out[:,1], out[:,4], m=:circle, c=:black, lab="Generalist", leg=false)
 xaxis!("Ratio of habitat quality", :log10)
-yaxis!("Average density", :log10)
+yaxis!("Average density", :log10, (10, 1000))
 
 savefig("figure04.pdf")

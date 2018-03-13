8030fbc12a60faee2fc04fe2c8dabced98519685
Dominique Caron <dominique.caron.1@umontreal.ca>
2018-03-13 10:11:33 -0400

    Update functions.jl

--- code/functions.jl
+++ code/functions.jl
@@ -1,1 +1,17 @@
 # list of functions
+"""
+***Insect population at next generation***
+
+- `N`: Initial host population size 
+- `P`: Initial parasitoid population size
+- `F`: Finite rate of increase of the host population
+- `D`: Density independent mortality (as a probability of survival)
+
+Return : `Nt`: Population size at the next generation
+"""
+
+function hostdynamic(N::Float64,P::Float64, F::Float64, D::Float64)
+    pescape = prop(N,P)
+    Nt = F*N*pescape*D
+    return Nt
+end

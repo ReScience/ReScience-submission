433a32ca81a7025d8e1fb5e4efbf9f9b704b7d91
Dominique Caron <dominique.caron.1@umontreal.ca>
2018-03-13 10:25:27 -0400

    add function 1 and 3

--- code/functions.jl
+++ code/functions.jl
@@ -1,4 +1,24 @@
 # list of functions
+
+"""
+***Probability of escaping mortality from natural ennemies***
+
+- `N`: Initial host population size 
+- `P`: Initial parasitoid population size
+- `a`: Searching efficiency (per capita)
+- `m`: Extent of clumping of the parasitoid attacks
+- `th`: Handling time (as a proportion of the total time)
+
+Return : `pescape`: Probability of escaping mortality from natural ennemies
+"""
+
+function prob(N::Float64,P::Float64, a::Float64, m::Float64, th::Float64)
+    num = a*P
+    den = m*(1+a*th*N)
+    pescape = (1+num/den)^-m
+    return pescape
+end
+
 """
 ***Insect population at next generation***
 
@@ -11,7 +31,7 @@ Return : `Nt`: Population size at the next generation
 """
 
 function hostdynamic(N::Float64,P::Float64, F::Float64, D::Float64)
-    pescape = prop(N,P)
+    pescape = prob(N,P)
     Nt = F*N*pescape*D
     return Nt
 end

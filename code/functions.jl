1bbd09c8c0af51a73c9f138c8b11634e93eb9e69
Timothée Poisot <timothee.poisot@umontreal.ca>
2018-03-15 11:46:56 -0400

    add the `simulation` function

--- code/functions.jl
+++ code/functions.jl
@@ -1,6 +1,27 @@
 """
 TODO
 """
+function simulation(N::Float64, P::Float64; t::Int64=50, f=specialist_dyn, F=4.0, D=0.5, c=1.0, a=0.5, h=10.0, b=25.0, th=0.0, m=0.2)
+    # Parameters
+    p = @NT(F=F, D=D, c=c, a=a, h=h, b=b, th=th, m=m)
+    # Matrix to store the output
+    dynamics = zeros(Float64, (t+1,3))
+    dynamics[1,2] = N
+    dynamics[1,3] = P
+    # Iterations
+    for current_time in 1:t
+        N_next, P_next = timestep(dynamics[current_time,2], dynamics[current_time,3], p; parasite_fun=f)
+        dynamics[current_time+1,1] = current_time
+        dynamics[current_time+1,2] = N_next
+        dynamics[current_time+1,3] = P_next
+    end
+    # Return
+    return dynamics
+end
+
+"""
+TODO
+"""
 function timestep(N::Float64, P::Float64, p; parasite_dyn=specialist_dyn)
     Nt = host_dyn(N, P, p)
     Pt = parasite_dyn(N, P, p)

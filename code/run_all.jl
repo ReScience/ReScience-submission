a0b74ca70722ab3d756691feb93d7f825eed60b4
Dominique Caron <dominique.caron.1@umontreal.ca>
2018-04-14 16:31:56 -0400

    fix - changes

--- code/run_all.jl
+++ code/run_all.jl
@@ -1,4 +1,14 @@
+using NamedTuples
+using Plots
+using StatPlots
+using Measures
+using Distributions
+
+include("functions.jl")
+
 include("figure3.jl")
 include("figure4.jl")
 include("figure5.jl")
 include("figure6.jl")
+include("figure7.jl")
+include("figure8.jl")

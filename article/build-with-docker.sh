30b7d8a11fd7599186a84f9df180d0336dca919e
thesamovar <d.goodman@imperial.ac.uk>
2018-05-25 17:18:21 +0100

    Everything working properly with Docker now (probably)

--- article/build-with-docker.sh
+++ article/build-with-docker.sh
@@ -1,12 +1,2 @@
 #!/bin/sh
-docker run \
-  -v `pwd`/:/root/ \
-  pandoc \
-  --standalone \
-  --filter /usr/bin/pandoc-crossref \
-  --latex-engine=xelatex \
-  --template=rescience-template.tex \
-  --biblatex --bibliography=bibliography.bib \
-  -M "crossrefYaml=/crossref.yaml" \
-  --output Hathway-Goodman-2018.pdf Hathway-Goodman-2018.md
-
+docker run -v `pwd`/:/root/ pandoc

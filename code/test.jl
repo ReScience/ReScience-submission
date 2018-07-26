ca9f6dc7d36fbbcfc9e6607d35d52b2f55654213
Timothée Poisot <timothee.poisot@umontreal.ca>
2018-07-26 10:34:26 -0400

    replace linreg by \

--- code/test.jl
+++ code/test.jl
@@ -13,24 +13,21 @@ for i in eachindex(cor_a)
     sd = i == 1 ? 0.0 : 0.5
     sim, params = simulation(50.0, 25.0, m=0.2, F=4.0, D=0.5, c=1.0, a=0.5, th= 0.0, m=0.5, f=specialist_dyn, a_sd=sd)
     kval = mapslices((r) -> kvalue_by_generation(r, params), sim, 2)
-    a, b = linreg(vec(log10.(sim[:,2])), vec(kval))
-    cor_a[i] = b
+    cor_a[i] = first(\(vec(log10.(sim[:,2])), vec(kval)))
 end
 
 for i in eachindex(cor_D)
     sd = i == 1 ? 0.0 : 0.5
     sim, params = simulation(50.0, 25.0, m=0.2, F=4.0, D=0.5, c=1.0, a=0.5, th= 0.0, m=0.5, f=specialist_dyn, D_sd=sd)
     kval = mapslices((r) -> kvalue_by_generation(r, params), sim, 2)
-    a, b = linreg(vec(log10.(sim[:,2])), vec(kval))
-    cor_D[i] = b
+    cor_D[i] = first(\(vec(log10.(sim[:,2])), vec(kval))
 end
 
 for i in eachindex(cor_c)
     sd = i == 1 ? 0.0 : 0.5
     sim, params = simulation(50.0, 25.0, m=0.2, F=4.0, D=0.5, c=1.0, a=0.5, th= 0.0, m=0.5, f=specialist_dyn, c_sd=sd)
     kval = mapslices((r) -> kvalue_by_generation(r, params), sim, 2)
-    a, b = linreg(vec(log10.(sim[:,2])), vec(kval))
-    cor_c[i] = b
+    cor_c[i] = first(\(vec(log10.(sim[:,2])), vec(kval))
 end
 
 pl_a = density(cor_a[2:end], xlim=(0,0.6), ylim=(0,10), frame=:origin, c=:black, fill=(0, :grey, 0.2), leg=false)
@@ -43,10 +40,3 @@ pl_c = density(cor_c[2:end], xlim=(0,0.6), ylim=(0,10), frame=:origin, c=:black,
 vline!(pl_c, [first(cor_c)], lw=2, ls=:dot, c=:grey)
 
 plot(pl_D, pl_c, pl_a, layout=(3,1))
-
-#=
-a, b = linreg(vec(log10.(sim3[:,2])), vec(kval3))
-f1 = (x) -> a + b*x
-fig5f
-plot!(fig5f, f1)
-=#

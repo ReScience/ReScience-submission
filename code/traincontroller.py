63fb8f89bb81d57b91bc94d6ba0ddf04d6f5a255
Léonard Blier <leonardb@fb.com>
2019-02-22 15:14:37 +0100

    last version of the code

--- code/traincontroller.py
+++ code/traincontroller.py
@@ -10,56 +10,26 @@ import sys
 from os.path import join, exists
 from os import mkdir, unlink, listdir, getpid
 from time import sleep
-from torch.multiprocessing import Process, Queue
 import torch
+
 import cma
-from models import Controller
+from models import MDRNNCell, VAE, Controller
+
 from tqdm import tqdm
 import numpy as np
 from utils.misc import RolloutGenerator, ASIZE, RSIZE, LSIZE
-from utils.misc import load_parameters
+from utils.misc import load_parameters, save_checkpoint
 from utils.misc import flatten_parameters
+from utils.history import History
 
-# parsing
-parser = argparse.ArgumentParser()
-parser.add_argument('--logdir', type=str, help='Where everything is stored.')
-parser.add_argument('--n-samples', type=int, help='Number of samples used to obtain '
-                    'return estimate.')
-parser.add_argument('--pop-size', type=int, help='Population size.')
-parser.add_argument('--target-return', type=float, help='Stops once the return '
-                    'gets above target_return')
-parser.add_argument('--display', action='store_true', help="Use progress bars if "
-                    "specified.")
-parser.add_argument('--max-workers', type=int, help='Maximum number of workers.',
-                    default=32)
-args = parser.parse_args()
+import logging
 
-# Max number of workers. M
-
-# multiprocessing variables
-n_samples = args.n_samples
-pop_size = args.pop_size
-num_workers = min(args.max_workers, n_samples * pop_size)
-time_limit = 1000
-
-# create tmp dir if non existent and clean it if existent
-tmp_dir = join(args.logdir, 'tmp')
-if not exists(tmp_dir):
-    mkdir(tmp_dir)
-else:
-    for fname in listdir(tmp_dir):
-        unlink(join(tmp_dir, fname))
-
-# create ctrl dir if non exitent
-ctrl_dir = join(args.logdir, 'ctrl')
-if not exists(ctrl_dir):
-    mkdir(ctrl_dir)
 
 
 ################################################################################
 #                           Thread routines                                    #
 ################################################################################
-def slave_routine(p_queue, r_queue, e_queue, p_index):
+def slave_routine(p_queue, r_queue, e_queue, p_index, logdir):
     """ Thread routine.
 
     Threads interact with p_queue, the parameters queue, r_queue, the result
@@ -82,39 +52,39 @@ def slave_routine(p_queue, r_queue, e_queue, p_index):
     :args p_index: the process index
     """
     # init routine
+    
+    # device = torch.device('cpu')
+    # redirect streams
+    tmp_dir = join(logdir, 'tmp', str(getpid()))
+    sys.stdout = open(tmp_dir + '.out', 'a')
+    sys.stderr = open(tmp_dir + '.err', 'a')
+
+
+    handler = logging.FileHandler(tmp_dir + '.out')    
+    logger = logging.getLogger('main_'+str(getpid()))
+    logger.setLevel(logging.INFO)
+    logger.addHandler(handler)
+
     gpu = p_index % torch.cuda.device_count()
     device = torch.device('cuda:{}'.format(gpu) if torch.cuda.is_available() else 'cpu')
-
-    # redirect streams
-    sys.stdout = open(join(tmp_dir, str(getpid()) + '.out'), 'a')
-    sys.stderr = open(join(tmp_dir, str(getpid()) + '.err'), 'a')
-
-    with torch.no_grad():
-        r_gen = RolloutGenerator(args.logdir, device, time_limit)
-
-        while e_queue.empty():
-            if p_queue.empty():
-                sleep(.1)
-            else:
-                s_id, params = p_queue.get()
-                r_queue.put((s_id, r_gen.rollout(params)))
-
-
-################################################################################
-#                Define queues and start workers                               #
-################################################################################
-p_queue = Queue()
-r_queue = Queue()
-e_queue = Queue()
-
-for p_index in range(num_workers):
-    Process(target=slave_routine, args=(p_queue, r_queue, e_queue, p_index)).start()
+    try:
+        with torch.no_grad():
+            time_limit = 1000
+            r_gen = RolloutGenerator(logdir, device, time_limit, logger)
+            while e_queue.empty():
+                if p_queue.empty():
+                    sleep(.1)
+                else:
+                    s_id, params = p_queue.get()
+                    r_queue.put((s_id, r_gen.rollout(params)))
+    except Exception:
+        logger.error(f"Fatal error in process {p_index}", exc_info=True)
 
 
 ################################################################################
 #                           Evaluation                                         #
 ################################################################################
-def evaluate(solutions, results, rollouts=100):
+def evaluate(param, rollouts, p_queue, r_queue):
     """ Give current controller evaluation.
 
     Evaluation is minus the cumulated reward averaged over rollout runs.
@@ -125,90 +95,213 @@ def evaluate(solutions, results, rollouts=100):
 
     :returns: minus averaged cumulated reward
     """
-    index_min = np.argmin(results)
-    best_guess = solutions[index_min]
-    restimates = []
+
+    results = []
 
     for s_id in range(rollouts):
-        p_queue.put((s_id, best_guess))
+        p_queue.put((s_id, param))
 
-    print("Evaluating...")
+    
     for _ in tqdm(range(rollouts)):
         while r_queue.empty():
             sleep(.1)
-        restimates.append(r_queue.get()[1])
+        results.append(r_queue.get()[1])
 
-    return best_guess, np.mean(restimates), np.std(restimates)
+    return np.mean(results), np.std(results)
 
-################################################################################
-#                           Launch CMA                                         #
-################################################################################
-controller = Controller(LSIZE, RSIZE, ASIZE)  # dummy instance
 
-# define current best and load parameters
-cur_best = None
-ctrl_file = join(ctrl_dir, 'best.tar')
-print("Attempting to load previous best...")
-if exists(ctrl_file):
-    state = torch.load(ctrl_file, map_location={'cuda:0': 'cpu'})
-    cur_best = - state['reward']
-    controller.load_state_dict(state['state_dict'])
-    print("Previous best was {}...".format(-cur_best))
+def init_random_models():
+    vae = VAE(3, LSIZE).to('cuda:0')
+    mdrnn = MDRNNCell(LSIZE, ASIZE, RSIZE, 5).to('cuda:0')
 
-parameters = controller.parameters()
-es = cma.CMAEvolutionStrategy(flatten_parameters(parameters), 0.1,
-                              {'popsize': pop_size})
+    for (model, dirname) in [(vae, 'vae'), (mdrnn, 'mdrnn')]:
+        print(f"reinitilizing {dirname}")
+        best_filename = join(args.logdir, dirname, 'best.tar')
+        if exists(best_filename):
+            continue
 
-epoch = 0
-log_step = 3
-while not es.stop():
-    if cur_best is not None and - cur_best > args.target_return:
-        print("Already better than target, breaking...")
-        break
+        if not exists(join(args.logdir, dirname)):
+            mkdir(join(args.logdir, dirname))
 
-    r_list = [0] * pop_size  # result list
-    solutions = es.ask()
+        filename = join(args.logdir, dirname, 'checkpoint.tar')
+        checkpoint = {'epoch': 0, 'state_dict': model.state_dict()}
+        if dirname in ['vae', 'mdrnn']:
+            checkpoint['precision'] = np.inf
+        else:
+            checkpoint['reward'] = -np.inf
+        save_checkpoint(checkpoint, True, filename, best_filename)
+    
+        
 
-    # push parameters to queue
-    for s_id, s in enumerate(solutions):
-        for _ in range(n_samples):
-            p_queue.put((s_id, s))
 
-    # retrieve results
-    if args.display:
-        pbar = tqdm(total=pop_size * n_samples)
-    for _ in range(pop_size * n_samples):
-        while r_queue.empty():
-            sleep(.1)
-        r_s_id, r = r_queue.get()
-        r_list[r_s_id] += r / n_samples
-        if args.display:
-            pbar.update(1)
-    if args.display:
-        pbar.close()
 
-    es.tell(solutions, r_list)
-    es.disp()
 
-    # evaluation and saving
-    if epoch % log_step == log_step - 1:
-        best_params, best, std_best = evaluate(solutions, r_list)
-        print("Current evaluation: {}".format(best))
-        if not cur_best or cur_best > best:
-            cur_best = best
-            print("Saving new best with value {}+-{}...".format(-cur_best, std_best))
-            load_parameters(best_params, controller)
-            torch.save(
-                {'epoch': epoch,
-                 'reward': - cur_best,
-                 'state_dict': controller.state_dict()},
-                join(ctrl_dir, 'best.tar'))
-        if - best > args.target_return:
-            print("Terminating controller training with value {}...".format(best))
+def main():
+    ################################################################################
+    # Initialization of the random models
+    init_random_models()
+
+    ################################################################################
+    #                Define queues and start workers                               #
+    ################################################################################
+    p_queue = mp.Queue()
+    r_queue = mp.Queue()
+    e_queue = mp.Queue()
+
+    for p_index in range(num_workers):
+        mp.Process(target=slave_routine, args=(p_queue, r_queue, e_queue, p_index, args.logdir)).start()
+
+
+    ################################################################################
+    #                           Launch CMA                                         #
+    ################################################################################
+    controller = Controller(LSIZE, RSIZE, ASIZE)  # dummy instance
+
+    # define current best and load parameters
+    cur_best = None
+    ctrl_file = join(ctrl_dir, 'best.tar')
+    mainlogger.info("Attempting to load previous best...")
+    epoch = 0
+    if not args.noreload and exists(ctrl_file):
+        state = torch.load(ctrl_file, map_location={'cuda:0': 'cpu'})
+        cur_best = - state['reward']
+        epoch = state["epoch"] + 1
+        controller.load_state_dict(state['state_dict'])
+        mainlogger.info("Previous best was {}...".format(-cur_best))
+        history.cut(epoch)
+
+    parameters = controller.parameters()
+    es = cma.CMAEvolutionStrategy(flatten_parameters(parameters), 0.1,
+                                  {'popsize': pop_size})
+
+    
+    log_step = 1
+    while not es.stop():
+        if cur_best is not None and - cur_best > args.target_return:
+            mainlogger.info("Already better than target, breaking...")
             break
 
 
-    epoch += 1
+        r_list = [0] * pop_size  # result list
+        mainlogger.info("Ask CMA")
+        solutions = es.ask()
 
-es.result_pretty()
-e_queue.put('EOP')
+        mainlogger.info("Put in the queue")
+        for s_id, s in enumerate(solutions):
+            for _ in range(n_samples):
+                p_queue.put((s_id, s))
+
+        # retrieve results
+        if args.display:
+            pbar = tqdm(total=pop_size * n_samples)
+        for t in range(pop_size * n_samples):
+            mainlogger.info(f"{t}/{pop_size * n_samples}")
+            while r_queue.empty():
+                sleep(.1)
+            r_s_id, r = r_queue.get()
+            r_list[r_s_id] += r / n_samples
+            if args.display:
+                pbar.update(1)
+        if args.display:
+            pbar.close()
+        mainlogger.info("Training completed. Now, CMA.")
+
+
+        history.push("Return", epoch, r_list)
+
+        es.tell(solutions, r_list)
+        es.disp()
+
+        # evaluation and saving
+        if epoch % log_step == log_step - 1:
+            history.dump()
+            _, best_params = min(enumerate(solutions), key=lambda idx_p: r_list[idx_p[0]])
+            best, std_best = evaluate(best_params, 100, p_queue, r_queue)
+
+            mainlogger.info("Current evaluation: {}".format(best))
+            if not cur_best or cur_best > best:
+                cur_best = best
+                mainlogger.info("Saving new best with value {}+-{}...".format(-cur_best, std_best))
+                load_parameters(best_params, controller)
+                torch.save(
+                    {'epoch': epoch,
+                     'reward': - cur_best,
+                     'state_dict': controller.state_dict()},
+                    join(ctrl_dir, 'best.tar'))
+            
+
+
+        epoch += 1
+        mainlogger.info("End of loop")
+    es.result_pretty()
+    
+
+
+
+
+if __name__ == '__main__':
+    # parsing
+    # torch.multiprocessing.set_start_method("spawn")
+    mp = torch.multiprocessing.get_context('spawn')
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--logdir', type=str, help='Where everything is stored.')
+    parser.add_argument('--n-samples', type=int, help='Number of samples used to obtain '
+                        'return estimate.')
+    parser.add_argument('--pop-size', type=int, help='Population size.')
+    parser.add_argument('--target-return', type=float, help='Stops once the return '
+                        'gets above target_return')
+    parser.add_argument('--display', action='store_true', help="Use progress bars if "
+                        "specified.")
+    parser.add_argument('--max-workers', type=int, help='Maximum number of workers.',
+                        default=32)
+    parser.add_argument('--noreload', action='store_true',
+                help='Restart from scratch for the controller')
+    parser.add_argument('--random-rnn', action='store_true',
+                help='Do not load a trained RNN but use a random one')
+    parser.add_argument('--random-vae', action='store_true',
+                help='Do not load the trained RNN VAE but use a random one')
+    args = parser.parse_args()
+
+    if args.random_rnn and args.random_vae:
+        subdir = 'untrainedrnnvae'
+    elif args.random_rnn and not args.random_vae:
+        subdir = 'untrainedrnn'
+    else:
+        subdir = 'standard'
+
+
+    # multiprocessing variables
+    n_samples = args.n_samples
+    pop_size = args.pop_size
+    num_workers = min(args.max_workers, n_samples * pop_size)
+    time_limit = 1000
+
+    # mainlogger.info(f"CUDA:{torch.cuda.is_available()}, NGPU:{torch.cuda.device_count()}")
+    # create tmp dir if non existent and clean it if existent
+    tmp_dir = join(args.logdir, 'tmp')
+    if not exists(tmp_dir):
+        mkdir(tmp_dir)
+    else:
+        for fname in listdir(tmp_dir):
+            unlink(join(tmp_dir, fname))
+
+
+
+
+
+    # create ctrl dir if non exitent
+    ctrl_dir = join(args.logdir, 'ctrl')
+    if not exists(ctrl_dir):
+        mkdir(ctrl_dir)
+    history = History(join(ctrl_dir, 'history.pkl'))
+
+    handler = logging.FileHandler(join(tmp_dir, 'mainlogger.out'))        
+    mainlogger = logging.getLogger('main_'+str(getpid()))
+    mainlogger.setLevel(logging.INFO)
+    mainlogger.addHandler(handler)
+
+    try:
+        main()
+    except Exception:
+        mainlogger.error(f"Fatal error in main", exc_info=True)
+    
